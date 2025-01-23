85d72bb98bac7ddbcb055137dd2c100171734014
anuj yadav <anujyadav.itengineer@gmail.com>
2025-01-23 15:54:35 +0530

    learned usecase of middleware

--- src/app.js
+++ src/app.js
@@ -4,67 +4,29 @@ const express = require("express");
 
 const app = express();
 
+const {adminAuth, userAuth} = require("./middlewares/auth");
 
-//app.use("/Route" , [RH1, RH2, RH3, RH4, RH5]);
+app.use("/admin" ,adminAuth );
 
-// get/users => middleware => routes handler
-
-app.use("/" , (req,res, next) =>{
-    console.log("first middleware");
-    next();
+app.get("/user/login" , (req,res,next) => {
+    res.send("User login successfully");
 })
 
-app.get("/user", [(req,res,next) =>{
-    console.log("handling the route user!");
-    //res.send("Response1!");
-    next();
-}, (req,res,next) =>{
-    console.log("handling route 2");
-    //res.send("response2!");
-    next();
-}],(req,res,next) =>{
-    console.log("handling route 3");
-    //res.send("response3!");
-    next();
-},(req,res,next) =>{
-    console.log("handling route 4");
-    //res.send("response4!");
-    next();
-},(req,res,next) =>{ // this function is a route handle rest every function is an middlware 
-    console.log("handling route 5");
-    //next();
-    res.send("response5!");
-    //next();
-},)
+app.get("/user/data" ,userAuth, (req,res,next) => {
+    res.send("User data send ");
+})
 
-
-/*
-// another way of writing the above code
-app.use("/user", (req,res,next) =>{
-    console.log("handling the route user1!");
-    //res.send("Response1!");
-    next();
+app.get("/admin/getAllData", (req,res) =>{
+    // authorising the request
+    res.send("All Data send");
+    
 });
 
-app.use("/user", (req,res,next) =>{
-    console.log("handling the route user2!");
-    //res.send("Response1!");
-    next();
+app.get("/admin/deleteUser", (req,res) =>{
+    // authorising the request
+    res.send("Deleted a user");
+   
 });
-
-app.use("/user", (req,res,next) =>{
-    console.log("handling the route user3!");
-    res.send("Response3!");
-    next();
-});
-
-*/
-
-// app.use("/getUserData" , (req,res)=>{
-//     //fetching the user data
-//     throw new Error("adfadfasd");
-//     res.send("user Data send");
-// });
 
 
 
--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,31 +1,29 @@
-const adminAuth = (req,res,next)=>{
-    
-    console.log("admin auth is checked");
 
+const adminAuth = (req,res,next) =>{
     const token = "xyz";
     const isAdminAuthorized = token === "xyz";
     if(!isAdminAuthorized){
-        res.status(401).send("admin is invalid");
+        res.status(401).send("unauthorized request");
     }
     else{
         next();
     }
 };
 
-const userAuth = (req,res,next)=>{
-    console.log("admin auth is checked");
+
+const userAuth = (req,res,next) =>{
     const token = "xyz";
-    const isAdminAuthorized = token === "xyzs";
+    const isAdminAuthorized = token === "xyz";
     if(!isAdminAuthorized){
-        res.status(401).send("admin is invalid");
+        res.status(401).send("unauthorized request");
     }
     else{
         next();
     }
-}
+};
 
-module.exports={
+
+module.exports = {
     adminAuth,
     userAuth,
-};
-   
+}
