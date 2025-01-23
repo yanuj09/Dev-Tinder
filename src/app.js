4d2c86768e1038ba82aa78934ffb511221108d2b
anuj yadav <anujyadav.itengineer@gmail.com>
2025-01-23 12:53:27 +0530

    middleware and route handler

--- src/app.js
+++ src/app.js
@@ -7,7 +7,14 @@ const app = express();
 
 //app.use("/Route" , [RH1, RH2, RH3, RH4, RH5]);
 
-app.use("/user", [(req,res,next) =>{
+// get/users => middleware => routes handler
+
+app.use("/" , (req,res, next) =>{
+    console.log("first middleware");
+    next();
+})
+
+app.get("/user", [(req,res,next) =>{
     console.log("handling the route user!");
     //res.send("Response1!");
     next();
@@ -23,12 +30,35 @@ app.use("/user", [(req,res,next) =>{
     console.log("handling route 4");
     //res.send("response4!");
     next();
-},(req,res,next) =>{
+},(req,res,next) =>{ // this function is a route handle rest every function is an middlware 
     console.log("handling route 5");
     //next();
     res.send("response5!");
     //next();
 },)
+
+
+/*
+// another way of writing the above code
+app.use("/user", (req,res,next) =>{
+    console.log("handling the route user1!");
+    //res.send("Response1!");
+    next();
+});
+
+app.use("/user", (req,res,next) =>{
+    console.log("handling the route user2!");
+    //res.send("Response1!");
+    next();
+});
+
+app.use("/user", (req,res,next) =>{
+    console.log("handling the route user3!");
+    res.send("Response3!");
+    next();
+});
+
+*/
 
 // app.use("/getUserData" , (req,res)=>{
 //     //fetching the user data
